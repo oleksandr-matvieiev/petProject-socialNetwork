da2e5221f7087328f0ddd442b08bfcf3df48cb0a
Oleksandr <oleksandr.matvieiev.d@gmail.com>
2025-01-03 13:28:16 +0200

    Make controller for edit profile info

--- src/main/java/org/petproject/socialnetwork/Controller/UserController.java
+++ src/main/java/org/petproject/socialnetwork/Controller/UserController.java
@@ -3,14 +3,15 @@ package org.petproject.socialnetwork.Controller;
 import org.petproject.socialnetwork.DTO.PostDTO;
 import org.petproject.socialnetwork.DTO.UserDTO;
 import org.petproject.socialnetwork.Mapper.UserMapper;
+import org.petproject.socialnetwork.Model.User;
+import org.petproject.socialnetwork.Service.AuthenticationService;
 import org.petproject.socialnetwork.Service.PostService;
 import org.petproject.socialnetwork.Service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -18,11 +19,13 @@ import java.util.List;
 public class UserController {
     private final PostService postService;
     private final UserService userService;
+    private final AuthenticationService authenticationService;
     private final UserMapper userMapper;
 
-    public UserController(PostService postService, UserService userService, UserMapper userMapper) {
+    public UserController(PostService postService, UserService userService, AuthenticationService authenticationService, UserMapper userMapper) {
         this.postService = postService;
         this.userService = userService;
+        this.authenticationService = authenticationService;
         this.userMapper = userMapper;
     }
 
@@ -37,6 +40,16 @@ public class UserController {
         UserDTO userDTO = userService.findUserByUsernameOrThrow(username);
         List<PostDTO> list = postService.getPostsByUser(userMapper.toEntity(userDTO));
         return ResponseEntity.ok(list);
+    }
+
+    @PostMapping("/edit/profile-info")
+    public ResponseEntity<UserDTO> editProfileInfo(
+            @RequestParam(required = false) String bio,
+            @RequestParam(required = false) MultipartFile newPicture
+    ) throws IOException {
+        User user = authenticationService.getCurrentUser();
+        UserDTO userDTO = userService.changeAccountInfo(user, bio, newPicture);
+        return ResponseEntity.ok(userDTO);
     }
 
 
